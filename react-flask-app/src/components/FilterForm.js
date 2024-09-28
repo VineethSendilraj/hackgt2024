09237fac229ea39e33fcb327adbd4eaf7288ece7
frankchang1000 <frank.chang1000@gmail.com>
2024-09-28 13:24:39 -0400

    filter form not working

---
+++ react-flask-app/src/components/FilterForm.js
@@ -0,0 +1,85 @@
+import { useEffect, useState } from 'react';
+
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Checkbox,
+  CheckboxGroup,
+  Input,
+  Stack,
+  SimpleGrid,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from '@chakra-ui/react';
+
+const FilterForm = ({ filters, setFilters, id }) => {
+  // on mount fill out the state
+  const [year, setYear] = useState(filters[id].year);
+  const [crime, setCrime] = useState(filters[id].crime);
+
+  const updateFilter = (c, e) => {
+    let updatedFilter = filters;
+    updatedFilter[id][c] = e;
+    setFilters({ ...updatedFilter });
+  };
+
+  const delFilter = () => {
+    let updatedFilter = filters;
+    delete updatedFilter[id];
+    setFilters({ ...updatedFilter });
+  };
+
+  return (
+    <>
+      <CheckboxGroup
+        onChange={e => {
+          updateFilter('year', e);
+        }}
+        defaultValue={filters[id].year}
+      >
+        <FormLabel>Year</FormLabel>
+        <SimpleGrid spacing={5} columns={2}>
+          <Checkbox value="2020">2020</Checkbox>
+          <Checkbox value="2021">2021</Checkbox>
+          <Checkbox value="2022">2022</Checkbox>
+          <Checkbox value="2023">2023</Checkbox>
+        </SimpleGrid>
+      </CheckboxGroup>
+      <CheckboxGroup
+        onChange={e => {
+          updateFilter('crime', e);
+        }}
+        defaultValue={filters[id].crime}
+      >
+        <FormLabel>Crime Involved</FormLabel>
+        <SimpleGrid spacing={5} columns={2}>
+          <Checkbox value="Aggravated Assault">Aggravated Assault</Checkbox>
+          <Checkbox value="Auto Theft">Auto Theft</Checkbox>
+          <Checkbox value="Larceny-From Vehicle">Larceny-From Vehicle</Checkbox>
+          <Checkbox value="Larceny-Non Vehicle">Larceny-Non Vehicle</Checkbox>
+          <Checkbox value="Burglary">Burglary</Checkbox>
+          <Checkbox value="Homicide">Homicide</Checkbox>
+          <Checkbox value="Robbery">Robbery</Checkbox>
+        </SimpleGrid>
+      </CheckboxGroup>
+      {Object.keys(filters).length > 1 ? (
+        <Button
+          colorScheme="red"
+          onClick={() => {
+            delFilter();
+          }}
+        >
+          Remove Filter
+        </Button>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+export default FilterForm;
