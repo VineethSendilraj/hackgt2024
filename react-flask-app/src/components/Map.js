df324105813541e90173612938329637625ae2a0
Vineeth Sendilraj <vineethsendilraj@gmail.com>
2024-09-28 02:10:34 -0400

    added variable for mapbox api key

--- react-flask-app/src/components/Map.js
+++ react-flask-app/src/components/Map.js
@@ -3,23 +3,27 @@ import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import axios from "axios";
 import MapboxGeocoding from "@mapbox/mapbox-sdk/services/geocoding";
-import './Map.scss'; // Import CSS file for custom styling
-import {Car, Footprints, Bike } from 'lucide-react'; // Import from react-icons
+import "./Map.scss"; // Import CSS file for custom styling
+import { Car, Footprints, Bike } from "lucide-react"; // Import from react-icons
 
 const Direction = () => {
   const mapContainerRef = useRef(null);
-  const [mapStyle, setMapStyle] = useState("mapbox://styles/mapbox/streets-v11");
+  const [mapStyle, setMapStyle] = useState(
+    "mapbox://styles/mapbox/streets-v11"
+  );
   const [origin, setOrigin] = useState("");
   const [destination, setDestination] = useState("");
   const [routeGeometry, setRouteGeometry] = useState(null);
-  const [mode, setMode] = useState('walking'); // Default mode is walking
+  const [mode, setMode] = useState("walking"); // Default mode is walking
   const mapRef = useRef();
+  const mapToken = process.env.REACT_APP_MAP_TOKEN;
   const geocodingClient = MapboxGeocoding({
-    accessToken: 'pk.eyJ1IjoiZnJhbmtjaGFuZzEwMDAiLCJhIjoiY20xbGFzcG1hMDNvaTJxbjY3a3N4NWw4dyJ9.W78DlIwDnlVOrCE5F1OnkQ',
+    accessToken: mapToken,
   });
 
+
   useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZnJhbmtjaGFuZzEwMDAiLCJhIjoiY20xbGFzcG1hMDNvaTJxbjY3a3N4NWw4dyJ9.W78DlIwDnlVOrCE5F1OnkQ';
+    mapboxgl.accessToken = mapToken;
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: mapStyle,
@@ -31,135 +35,134 @@ const Direction = () => {
       // Clustering logic, route drawing, etc.
       // Same as in your current setup
       // Clustering logic
-      mapRef.current.addSource('crimes', {
-        type: 'geojson',
-        data: 'https://raw.githubusercontent.com/VineethSendilraj/hackgt2024/main/react-flask-app/src/components/data_1.geojson',
+      mapRef.current.addSource("crimes", {
+        type: "geojson",
+        data: "https://raw.githubusercontent.com/VineethSendilraj/hackgt2024/main/react-flask-app/src/components/data_1.geojson",
         cluster: true,
         clusterMaxZoom: 14,
-        clusterRadius: 75
+        clusterRadius: 75,
       });
 
       mapRef.current.addLayer({
-        id: 'clusters',
-        type: 'circle',
-        source: 'crimes',
-        filter: ['has', 'point_count'],
+        id: "clusters",
+        type: "circle",
+        source: "crimes",
+        filter: ["has", "point_count"],
         paint: {
-          'circle-color': [
-            'step',
-            ['get', 'point_count'],
-            '#51bbd6',
+          "circle-color": [
+            "step",
+            ["get", "point_count"],
+            "#51bbd6",
             100,
-            '#f1f075',
+            "#f1f075",
             750,
-            '#f28cb1'
+            "#f28cb1",
           ],
-          'circle-radius': [
-            'step',
-            ['get', 'point_count'],
+          "circle-radius": [
+            "step",
+            ["get", "point_count"],
             25, // Increased base size for a modern look
             100,
             35,
             750,
-            45
+            45,
           ],
-          'circle-opacity': 0.8, // Slight transparency for a softer look
-          'circle-stroke-width': 2, // Adding a stroke width
-          'circle-stroke-color': '#fff', // Stroke color
-          'circle-stroke-opacity': 0.6 // Slight transparency on the stroke
-        }
+          "circle-opacity": 0.8, // Slight transparency for a softer look
+          "circle-stroke-width": 2, // Adding a stroke width
+          "circle-stroke-color": "#fff", // Stroke color
+          "circle-stroke-opacity": 0.6, // Slight transparency on the stroke
+        },
       });
 
       mapRef.current.addLayer({
-        id: 'cluster-count',
-        type: 'symbol',
-        source: 'crimes',
-        filter: ['has', 'point_count'],
+        id: "cluster-count",
+        type: "symbol",
+        source: "crimes",
+        filter: ["has", "point_count"],
         layout: {
-          'text-field': ['get', 'point_count_abbreviated'],
-          'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
-          'text-size': 14, // Increased text size for better visibility
-          'text-color': '#fff' // Modern white text for contrast
-        }
+          "text-field": ["get", "point_count_abbreviated"],
+          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+          "text-size": 14, // Increased text size for better visibility
+          "text-color": "#fff", // Modern white text for contrast
+        },
       });
 
       mapRef.current.addLayer({
-        id: 'unclustered-point',
-        type: 'circle',
-        source: 'crimes',
-        filter: ['!', ['has', 'point_count']],
+        id: "unclustered-point",
+        type: "circle",
+        source: "crimes",
+        filter: ["!", ["has", "point_count"]],
         paint: {
-          'circle-color': '#11b4da',
-          'circle-radius': 6, // Slightly larger radius for unclustered points
-          'circle-opacity': 0.9, // Higher opacity for visibility
-          'circle-stroke-width': 2,
-          'circle-stroke-color': '#fff',
-          'circle-stroke-opacity': 0.6
-        }
+          "circle-color": "#11b4da",
+          "circle-radius": 6, // Slightly larger radius for unclustered points
+          "circle-opacity": 0.9, // Higher opacity for visibility
+          "circle-stroke-width": 2,
+          "circle-stroke-color": "#fff",
+          "circle-stroke-opacity": 0.6,
+        },
       });
 
       mapRef.current.addLayer({
-        id: 'unclustered-point',
-        type: 'circle',
-        source: 'crimes',
-        filter: ['!', ['has', 'point_count']],
+        id: "unclustered-point",
+        type: "circle",
+        source: "crimes",
+        filter: ["!", ["has", "point_count"]],
         paint: {
-          'circle-color': '#11b4da',
-          'circle-radius': 6,
-          'circle-opacity': 0.9,
-          'circle-stroke-width': 2,
-          'circle-stroke-color': '#fff',
-          'circle-stroke-opacity': 0.6
-        }
+          "circle-color": "#11b4da",
+          "circle-radius": 6,
+          "circle-opacity": 0.9,
+          "circle-stroke-width": 2,
+          "circle-stroke-color": "#fff",
+          "circle-stroke-opacity": 0.6,
+        },
       });
-  
 
       // Inspect a cluster on click
-      mapRef.current.on('click', 'clusters', (e) => {
+      mapRef.current.on("click", "clusters", (e) => {
         const features = mapRef.current.queryRenderedFeatures(e.point, {
-          layers: ['clusters']
+          layers: ["clusters"],
         });
         const clusterId = features[0].properties.cluster_id;
         mapRef.current
-          .getSource('crimes')
+          .getSource("crimes")
           .getClusterExpansionZoom(clusterId, (err, zoom) => {
             if (err) return;
 
             mapRef.current.easeTo({
               center: features[0].geometry.coordinates,
-              zoom: zoom
+              zoom: zoom,
             });
           });
       });
 
       // Popup for unclustered points
-      mapRef.current.on('click', 'unclustered-point', (e) => {
+      mapRef.current.on("click", "unclustered-point", (e) => {
         const coordinates = e.features[0].geometry.coordinates.slice();
         const properties = e.features[0].properties;
-      
+
         // Create HTML content for the popup
-        let popupContent = '';
+        let popupContent = "";
         Object.entries(properties).forEach(([key, value]) => {
           popupContent += `${key}: ${value}<br>`;
         });
-      
+
         // Adjust coordinates for the popup
         while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
           coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
         }
-      
+
         new mapboxgl.Popup()
           .setLngLat(coordinates)
           .setHTML(popupContent) // Set the HTML content with all properties
           .addTo(mapRef.current);
       });
-      
+
       // Change cursor style on mouse enter/leave
-      mapRef.current.on('mouseenter', 'clusters', () => {
-        mapRef.current.getCanvas().style.cursor = 'pointer';
+      mapRef.current.on("mouseenter", "clusters", () => {
+        mapRef.current.getCanvas().style.cursor = "pointer";
       });
-      mapRef.current.on('mouseleave', 'clusters', () => {
-        mapRef.current.getCanvas().style.cursor = '';
+      mapRef.current.on("mouseleave", "clusters", () => {
+        mapRef.current.getCanvas().style.cursor = "";
       });
 
       // Route geometry
@@ -186,12 +189,15 @@ const Direction = () => {
           },
         });
 
-        mapRef.current.fitBounds(routeGeometry.coordinates.reduce(
-          (bounds, coord) => bounds.extend(coord),
-          new mapboxgl.LngLatBounds()
-        ), {
-          padding: 50,
-        });
+        mapRef.current.fitBounds(
+          routeGeometry.coordinates.reduce(
+            (bounds, coord) => bounds.extend(coord),
+            new mapboxgl.LngLatBounds()
+          ),
+          {
+            padding: 50,
+          }
+        );
       }
     });
 
@@ -205,21 +211,30 @@ const Direction = () => {
   const calculateRoute = async () => {
     if (origin && destination) {
       try {
-        const originResponse = await geocodingClient.forwardGeocode({
-          query: origin,
-          limit: 1,
-        }).send();
+        const originResponse = await geocodingClient
+          .forwardGeocode({
+            query: origin,
+            limit: 1,
+          })
+          .send();
 
-        const destinationResponse = await geocodingClient.forwardGeocode({
-          query: destination,
-          limit: 1,
-        }).send();
+        const destinationResponse = await geocodingClient
+          .forwardGeocode({
+            query: destination,
+            limit: 1,
+          })
+          .send();
 
         const originCoordinates = originResponse.body.features[0].center;
-        const destinationCoordinates = destinationResponse.body.features[0].center;
+        const destinationCoordinates =
+          destinationResponse.body.features[0].center;
 
         const directionsResponse = await axios.get(
-          `https://api.mapbox.com/directions/v5/mapbox/driving/${originCoordinates.join(',')};${destinationCoordinates.join(',')}?geometries=geojson&access_token=${mapboxgl.accessToken}`
+          `https://api.mapbox.com/directions/v5/mapbox/driving/${originCoordinates.join(
+            ","
+          )};${destinationCoordinates.join(
+            ","
+          )}?geometries=geojson&access_token=${mapboxgl.accessToken}`
         );
 
         const route = directionsResponse.data.routes[0].geometry;
@@ -254,31 +269,30 @@ const Direction = () => {
 
         <div className="mode-buttons">
           <button
-            className={`mode-button ${mode === 'driving' ? 'selected' : ''}`}
-            onClick={() => handleModeChange('driving')}
+            className={`mode-button ${mode === "driving" ? "selected" : ""}`}
+            onClick={() => handleModeChange("driving")}
           >
             <Car />
           </button>
           <button
-            className={`mode-button ${mode === 'walking' ? 'selected' : ''}`}
-            onClick={() => handleModeChange('walking')}
+            className={`mode-button ${mode === "walking" ? "selected" : ""}`}
+            onClick={() => handleModeChange("walking")}
           >
             <Footprints />
           </button>
           <button
-            className={`mode-button ${mode === 'cycling' ? 'selected' : ''}`}
-            onClick={() => handleModeChange('cycling')}
+            className={`mode-button ${mode === "cycling" ? "selected" : ""}`}
+            onClick={() => handleModeChange("cycling")}
           >
             <Bike />
           </button>
           <button onClick={calculateRoute}>Get Directions</button>
         </div>
-
       </div>
       <div
         ref={mapContainerRef}
         className="map"
-        style={{ height: '100vh' }} // Ensure the map takes full height
+        style={{ height: "100vh" }} // Ensure the map takes full height
       />
     </div>
   );
